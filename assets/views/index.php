a13b11db5a9f289e326e12d2031a8a977f4c7f06
William <william@lawnstewart.com>
2014-02-17 23:12:09 +1300

    ajax errors now displayed properly removed incorrect db constraint

--- assets/views/index.php
+++ assets/views/index.php
@@ -10,7 +10,7 @@
 	<script src="/js/jquery-1.8.2.min.js" type="text/javascript"></script>
 	<script src="/js/jquery.ba-bbq.min.js" type="text/javascript"></script>
 	<script src="/js/jquery-ui-1.10.3.custom.min.js" type="text/javascript"></script>
-	<script src="/js/ajaxprovider.js?2" type="text/javascript"></script>
+	<script src="/js/ajaxprovider.js?3" type="text/javascript"></script>
 	<script src="/js/jquery.jtable.min.js" type="text/javascript"></script>
 
 </head>
