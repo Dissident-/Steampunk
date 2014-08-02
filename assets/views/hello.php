0c250568385851eae30539861ec35917a9c7f2cf
William <william@lawnstewart.com>
2014-08-03 01:11:20 +1200

    The hello message on the brawl site

---
+++ assets/views/hello.php
@@ -0,0 +1,3 @@
+<p>The Brawl is inspired by <a rel="no_ajax" href="http://www.nexusclash.com">Nexus Clash</a>, which was inspired by <a rel="no_ajax" href="http://www.nexuswar.com">Nexus War</a>.</p>
+
+<p>Thanks to Kandarin for suggesting a small map to keep people murderising while NC is down for maintenance.</p>
